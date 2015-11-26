fdc8df00b8f135c55d941ceec081c7e7ec4a22ca
刘泉 <liuq@uc888.cn>
2015-11-26 13:36:50 +0800

    change default log to ignore SET ENV LOGSPOUT=true then log enabled

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -e
+export http_proxy=http://192.168.21.21:8080
+export https_proxy=http://192.168.21.21:8080
 apk add --update go git mercurial
 mkdir -p /go/src/github.com/gliderlabs
 cp -r /src /go/src/github.com/gliderlabs/logspout
@@ -10,6 +12,9 @@ go build -ldflags "-X main.Version $1" -o /bin/logspout
 apk del go git mercurial
 rm -rf /go
 rm -rf /var/cache/apk/*
+
+unset http_proxy
+unset https_proxy
 
 # backwards compatibility
 ln -fs /tmp/docker.sock /var/run/docker.sock
--- custom/modules.go
+++ custom/modules.go
@@ -3,6 +3,6 @@ package main
 import (
 	_ "github.com/gliderlabs/logspout/adapters/syslog"
 	_ "github.com/gliderlabs/logspout/transports/tcp"
-	_ "github.com/gliderlabs/logspout-logstash"
-        _ "github.com/gliderlabs/logspout/transports/udp"
+	_ "github.com/liubaishui/logspout-logstash"
+    _ "github.com/gliderlabs/logspout/transports/udp"
 )
