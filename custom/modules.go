926e4310cd488b2a0e476c5b657699eb7e9457fe
liubaishui <szliuquan@gmail.com>
2015-11-26 10:59:49 +0800

    Update modules.go

--- custom/modules.go
+++ custom/modules.go
@@ -3,4 +3,6 @@ package main
 import (
 	_ "github.com/gliderlabs/logspout/adapters/syslog"
 	_ "github.com/gliderlabs/logspout/transports/tcp"
+	_ "github.com/gliderlabs/logspout-logstash"
+        _ "github.com/gliderlabs/logspout/transports/udp"
 )
